e07baffb70b0bb8af1bdaf755c159af495769853
Joris Bertier <joris.bertier@gmail.com>
2024-10-29 16:36:00 -0600

    first commit

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux';
+import { store } from './store';
+import './App.css';
+
+function App() {
+  return (
+    <Provider store={store}>
+        <div className="App">
+        </div>
+    </Provider>
+  );
+}
+  
+export default App;
---
+++ src/common/models.js
@@ -0,0 +1,15 @@
+const DoubleCantal = {
+    title: 'Double Cantal',
+    price: 15.99,
+}
+
+
+const SuperCremeux = {
+    title: 'Super Crémeux',
+    price: 14.99,
+}
+
+const PouletCroquant = {
+    title: 'Poulet Croquant',
+    price: 17.99,
+}
